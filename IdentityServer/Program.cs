cf52a37ace62c3a7b211cf9831d85b896027d74d
Maidul Islam <maidul.bd@asa-international.com>
2022-05-18 10:01:43 +0600

    Identity Server Project added and configuration added

--- IdentityServer/Program.cs
+++ IdentityServer/Program.cs
@@ -1,6 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var assembly = typeof(Program).Assembly.GetName().Name;
+var CoffeeShopperConnection = builder.Configuration.GetConnectionString("CoffeeShopperConnection");
+
+builder.Services.AddIdentityServer()
+    .AddConfigurationStore(options =>
+    {
+        options.ConfigureDbContext = c => c.UseSqlServer(CoffeeShopperConnection,
+            opt => opt.MigrationsAssembly(assembly));
+    })
+    .AddOperationalStore(options =>
+    {
+        options.ConfigureDbContext = c => c.UseSqlServer(CoffeeShopperConnection,
+            opt => opt.MigrationsAssembly(assembly));
+    })
+    .AddDeveloperSigningCredential();
+
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//app.MapGet("/", () => "Hello World!");
+
+app.UseIdentityServer();
 
 app.Run();
